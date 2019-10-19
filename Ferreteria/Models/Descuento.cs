97f4ca750160377a52768898ff30be7ff9f41cd0
psep <psep.pentauc@gmail.com>
2019-10-19 00:02:55 -0300

    initial commit

---
+++ Ferreteria/Models/Descuento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ferreteria.Models
+{
+    public class Descuento
+    {
+        public long IdDescuento { set; get; }
+        public int Porcentaje { set; get; }
+
+    }
+}
